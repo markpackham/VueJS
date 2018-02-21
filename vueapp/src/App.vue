b61986fa9b3449c324bc06bbec5d2fb4a757ea23
Mark Packham <markpackham1@gmail.com>
2018-02-21 15:55:44 +0000

    learning basics

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div id="app">
+        <test/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+    import Test from './components/test'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Test
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ vueapp/src/components/Test.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="test">
+        <!--you can edit the title -->
+        <input type="text" v-model="title"><br/>
+        <h1>{{title}}</h1>
+        <p>{{user.firstName}}</p>
+        <p v-text="user.firstName"></p>
+        <p v-if="showName">{{user.firstName}}</p>
+        <p v-else>Name not found</p>
+        <ul>
+            <li v-for="item in items">{{item.title}}</li>
+        </ul>
+        <button v-on:click="greet('Enatro Adoon')">
+            Say Greeting
+        </button>
+        <br>
+        <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
+        <hr>
+        <label>First Name: </label>
+        <input type="text" v-model="user.firstName">
+        <br>
+        <label>Last Name: </label>
+        <input type="text" v-model="user.lastName">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test',
+        data() {
+            return {
+                title: 'Hello Mark',
+                user: {
+                    firstName: 'John',
+                    lastName: 'Smith'
+                },
+                showName: true,
+                items: [
+                    {title: 'Item One'},
+                    {title: 'Item Two'},
+                    {title: 'Item Three'}
+                ]
+            }
+        },
+        methods: {
+            greet: function (greeting) {
+                alert(greeting);
+            },
+            pressKey: function (e) {
+                console.log(e.target.value);
+            },
+            enterHit: function () {
+                console.log('You hit enter');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
