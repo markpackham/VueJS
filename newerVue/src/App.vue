39972dad2cb4e7422f5dc49ee8eb6d25ef3a8b43
Mark Packham <markpackham1@gmail.com>
2019-01-10 20:19:36 +0000

    Do more up to date tutorial on Vue.js from https://youtu.be/Wy9q22isx3U?t=1695

---
+++ newerVue/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+
+import Todos from './components/Todos';
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data(){
+    return{
+      todos:[
+        {
+          id: 1,
+title:"Meow",
+completed:false
+        },
+                {
+          id: 2,
+title:"Moo",
+completed:true
+        },
+                {
+          id: 3,
+title:"Baa",
+completed:false
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
+</style>
